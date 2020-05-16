12072468149008049fb4d2061de6c65c11b8b9b0
Utkarsh Srivastava <utkarshutt2706@gmail.com>
2020-05-16 23:16:04 +0530

    sync DB connection

--- app/index.js
+++ app/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 
-const logger = require('./utility/logger');
-const date = require('./utility/date');
 const createConnection = require('./utility/db');
 const errorHandler = require('./utility/errorHandler');
 const config = require('./config/development.json');
@@ -14,9 +12,6 @@ const marketRateRoute = require('./marketRate/marketRateRoute');
 const newsletter = require('./newsletter/newsletterController');
 const weather = require('./weather/weatherController');
 
-// get connection to DB
-createConnection();
-
 const app = express();
 
 // middlewares
@@ -27,7 +22,8 @@ app.use(express.static('public'))
 app.get('/', (req, resp) => {
     resp.writeHead(301, {'Location': 'https://kisan-sewa.herokuapp.com/'});
     resp.end();
-})
+});
+
 app.use('/auth', authRoute);
 app.use('/booth', boothRoute);
 app.use('market-rate', marketRateRoute);
@@ -40,7 +36,8 @@ app.use(errorHandler);
 // port number
 const port = process.env.PORT || config.port;
 
-app.listen(port, () => {
+app.listen(port, async () => {
     // logger.info(`${date()}: Server started on port number: ${port}.`);
+    await createConnection();
     console.log(`Server started at port number: ${port}`);
 });
